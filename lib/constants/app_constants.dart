ce222aaa47bd1244d57445c3be3d27b418bb74b8
Gökdeniz SARİİBRAHİM <gkdnzsrbrhm@gmail.com>
2022-08-17 14:50:09 +0300

    Create app_constants.dart

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class stabil {
+  static const mainColor = Colors.purple;
+  static const String mainText = "Calculate Average";
+
+  static final TextStyle mainStyle = GoogleFonts.quicksand(
+      fontSize: 24, fontWeight: FontWeight.w700, color: mainColor);
+}
