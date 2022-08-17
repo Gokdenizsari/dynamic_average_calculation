5613d754908b39a5cfafd6aee215e1ea3af65e43
Gökdeniz SARİİBRAHİM <gkdnzsrbrhm@gmail.com>
2022-08-17 17:17:11 +0300

    Update app_constants.dart

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -8,5 +8,13 @@ class stabil {
   static final TextStyle mainStyle = GoogleFonts.quicksand(
       fontSize: 24, fontWeight: FontWeight.w700, color: mainColor);
 
+static final TextStyle lessonStyle = GoogleFonts.quicksand(
+      fontSize: 14, fontWeight: FontWeight.w500, color: mainColor);
+
+      static final TextStyle averageStyle = GoogleFonts.quicksand(
+      fontSize: 40, fontWeight: FontWeight.w800, color: mainColor);
+      static final TextStyle averageStyle2 = GoogleFonts.quicksand(
+      fontSize: 24, fontWeight: FontWeight.w700, color: mainColor);
+
   static BorderRadius borderRadius = BorderRadius.circular(24);
 }
