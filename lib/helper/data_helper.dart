485ede634b8dc93f4488c6b25477007f5ffdf593
Gökdeniz SARİİBRAHİM <gkdnzsrbrhm@gmail.com>
2022-08-20 16:53:41 +0300

    Create data_helper.dart

---
+++ lib/helper/data_helper.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class DataHelper {
+  static List<String> _allLectureNotes() {
+    return ['AA', 'BA', 'BB', 'CB', 'CC', 'DC', 'DD', 'FD', 'FF'];
+  }
+
+  static double _numberEquivalentOfNotes(String character) {
+    switch (character) {
+      case 'AA':
+        return 4;
+      case 'BA':
+        return 3.5;
+      case 'BB':
+        return 3;
+      case 'CB':
+        return 2.5;
+      case 'CC':
+        return 2;
+      case 'DC':
+        return 1.5;
+      case 'DD':
+        return 1;
+      case 'FD':
+        return 0.5;
+      case 'FF':
+        return 0.0;
+      default:
+        return 1;
+    }
+  }
+
+  static List<DropdownMenuItem<double>> allLectureNotes() {
+    return _allLectureNotes()
+        .map(
+          (e) => DropdownMenuItem(
+            child: Text(e),
+            value: _numberEquivalentOfNotes(e),
+          ),
+        )
+        .toList();
+  }
+}
