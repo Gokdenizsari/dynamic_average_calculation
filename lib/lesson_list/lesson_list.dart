02a299392bb0c4a6fb02553670c2a9901f87a2da
Gökdeniz SARİİBRAHİM <gkdnzsrbrhm@gmail.com>
2022-08-22 22:46:24 +0300

    Create lesson_list.dart

---
+++ lib/lesson_list/lesson_list.dart
@@ -0,0 +1,61 @@
+import 'package:dynamic_average_calculation/constants/app_constants.dart';
+import 'package:dynamic_average_calculation/helper/data_helper.dart';
+import 'package:dynamic_average_calculation/model/lesson.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class LessonList extends StatefulWidget {
+  const LessonList({Key? key}) : super(key: key);
+
+  @override
+  State<LessonList> createState() => _LessonListState();
+}
+
+class _LessonListState extends State<LessonList> {
+  @override
+  Widget build(BuildContext context) {
+    List<Lesson> allLesson = DataHelper.allAddLesson;
+    setState(() {
+      
+    });
+
+    return allLesson.length > 0
+        ? ListView.builder(
+            itemCount: allLesson.length,
+            itemBuilder: (context, index) {
+              return Dismissible(
+                key: UniqueKey(),
+                direction: DismissDirection.startToEnd,
+                onDismissed: (c) {
+                  DataHelper.allAddLesson.removeAt(index);
+                  
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: Card(
+                    child: ListTile(
+                      title: Text(allLesson[index].id),
+                      leading: CircleAvatar(
+                        backgroundColor: stabil.mainColor,
+                        child: Text(
+                          (allLesson[index].letterValue *
+                                  allLesson[index].creditValue)
+                              .toStringAsFixed(0),
+                        ),
+                      ),
+                      subtitle: Text(
+                          "${allLesson[index].creditValue} Credit, Not Value ${allLesson[index].letterValue}"),
+                    ),
+                  ),
+                ),
+              );
+            })
+        : Container(
+            child: Center(
+                child: Text(
+              "Please enter a lesson",
+              style: stabil.mainStyle,
+            )),
+          );
+  }
+}
